4f63acb97816df3861e05de112f0812af78835c8
Arun Prabhu <arunprab21@gmail.com>
2024-01-30 14:59:04 -0500

    terraform code

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "google" {
   project = "enhanced-casing-278912"
-  region = "northamerica-northeast1"
+  region  = "northamerica-northeast1"
 }
 
 resource "google_storage_bucket" "my-bucket" {
-  name = "test-githubdemo-bucket"
-  location = "CA"
-  force_destroy = true
+  name                     = "test-githubdemo-bucket"
+  location                 = "CA"
+  force_destroy            = true
   public_access_prevention = "enforced"
 }
